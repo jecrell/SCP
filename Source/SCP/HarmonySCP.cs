5b06d753b1af1405567d71c7074159d00594baa6
Matthew Walls <matt.walls31@gmail.com>
2019-04-07 23:23:43 -0700

    Added SCP131AB

---
+++ Source/SCP/HarmonySCP.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using System.Reflection;
+using UnityEngine;
+using Verse.AI.Group;
+
+namespace SCP
+{
+
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        
+        static HarmonyPatches()
+        {
+            var harmony = HarmonyInstance.Create("rimworld.scp");
+            //harmony.Patch(AccessTools.Method(typeof(PawnUIOverlay), "DrawPawnGUIOverlay"),
+            //    null, new HarmonyMethod(typeof(HarmonyPatches), nameof(DrawPawnGUIOverlay)));
+        }
+
+        
+    }
+}
