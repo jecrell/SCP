c52079dd56d2838456537bb6a8f7887de3753fb4
Matthew Walls <matt.walls31@gmail.com>
2019-04-25 20:11:00 -0700

    Added SCP tab

--- Source/SCP/CustomPawnKindDef.cs
+++ Source/SCP/CustomPawnKindDef.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 
 namespace SCP
 {
+    public enum ObjectClass
+    {
+        Safe,
+        Euclid,
+        Keter,
+        Thaumiel,
+        Neutralized
+    }
+
+
+    public static class ObjectClassUtility
+    {
+        public static string GetObjectClassString(this ObjectClass objectClass)
+        {
+            return ("SCP_Class" + objectClass.ToString()).Translate();
+        }
+
+        public static string GetObjectClassDesc(this ObjectClass objectClass)
+        {
+            return ("SCP_Class" + objectClass.ToString() + "Desc").Translate();
+        }
+    }
+
     public class CustomPawnKindDef : Verse.PawnKindDef
     {
         public bool isUnique = false;
-        public bool trainableMechanoid = false;
+        public bool isSCP = true;        public bool trainableMechanoid = false;
+        public ObjectClass objectClass = ObjectClass.Safe;
     }
 }
